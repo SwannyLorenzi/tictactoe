e1336e87db9e10747aeb6259937f2f8f0407ff12
Swanny Lorenzi <slorenzi@ippon.fr>
2022-04-22 12:14:24 +0200

    add some unit tests to get started with pytest

---
+++ test_main.py
@@ -0,0 +1,12 @@
+import pytest
+
+from main import *
+
+
+@pytest.mark.parametrize("idx,expected", [(0, (1, 1)), (1, (2, 1)), (3, (1, 2))])
+def test_get_xy(idx, expected):
+    assert get_xy(idx) == expected
+
+
+def test_has_idx():
+    assert has_idx('X', ['X'], 0)
