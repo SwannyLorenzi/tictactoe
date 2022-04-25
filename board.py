6d2a998966acd1e7f98b3335b05fa8fdaf54e5f0
Swanny Lorenzi <slorenzi@ippon.fr>
2022-04-25 12:39:44 +0200

    Board.__str__

--- board.py
+++ board.py
@@ -1,6 +1,25 @@
+from math import log10
+
+
 class Board:
 
     def __init__(self, width: int = 3, height: int = 3):
         self.width = width
         self.height = height
         self.cells = [' '] * (width * height)
+
+    def __str__(self):
+        idx_pad = 1 + int(log10(self.width * self.height))
+        row_sep = '\n' + '-+-'.join(['-' * (3 + idx_pad)] * self.width) + '\n'
+
+        rows = []
+        index = 1
+        for r in range(0, self.height):
+            cells = []
+            for c in range(0, self.width):
+                val = self.cells[r * self.width + c]
+                idx = str(index).rjust(idx_pad, ' ')
+                cells.append(f'{idx}: {val}')
+                index += 1
+            rows.append(' | '.join(cells))
+        return row_sep.join(rows)
--- test_board.py
+++ test_board.py
@@ -4,28 +4,80 @@ from board import Board
 
 
 @pytest.mark.parametrize('width,height', [(3, 3), (4, 4)])
-class TestBoard:
+class TestBoardInit:
 
-    def test_board_width(self, width, height):
-        # Given
-        # When
-        board = Board(width, height)
+    @pytest.fixture()
+    def board(self, width, height):
+        return Board(width, height)
 
-        # Then
+    def test_board_width(self, board, width):
         assert board.width == width
 
-    def test_board_height(self, width, height):
-        # Given
-        # When
-        board = Board(width, height)
-
-        # Then
+    def test_board_height(self, board, height):
         assert board.height == height
 
-    def test_board_cells(self, width, height):
+    def test_board_cells(self, board, width, height):
+        assert board.cells == [' '] * width * height
+
+
+class TestBoardStr:
+
+    def test_str_3x3(self):
         # Given
+        board = Board()
+
         # When
-        board = Board(width, height)
+        board_str = str(board)
 
         # Then
-        assert board.cells == [' '] * width * height
+        assert board_str == '''1:   | 2:   | 3:  
+-----+------+-----
+4:   | 5:   | 6:  
+-----+------+-----
+7:   | 8:   | 9:  '''
+
+    def test_str_3x3_partially_filled(self):
+        # Given
+        board = Board()
+        board.cells[0] = 'X'
+        board.cells[4] = 'O'
+        board.cells[2] = 'X'
+
+        # When
+        board_str = str(board)
+
+        # Then
+        assert board_str == '''1: X | 2:   | 3: X
+-----+------+-----
+4:   | 5: O | 6:  
+-----+------+-----
+7:   | 8:   | 9:  '''
+
+    def test_str_3x3_totally_filled(self):
+        # Given
+        board = Board()
+        board.cells = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O']
+
+        # When
+        board_str = str(board)
+
+        # Then
+        assert board_str == '''1: X | 2: O | 3: X
+-----+------+-----
+4: O | 5: X | 6: X
+-----+------+-----
+7: O | 8: X | 9: O'''
+
+    def test_str_4x3(self):
+        # Given
+        board = Board(width=4)
+
+        # When
+        board_str = str(board)
+
+        # Then
+        assert board_str == ''' 1:   |  2:   |  3:   |  4:  
+------+-------+-------+------
+ 5:   |  6:   |  7:   |  8:  
+------+-------+-------+------
+ 9:   | 10:   | 11:   | 12:  '''
